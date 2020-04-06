c95939df56a35a25f9b0c25a1b5ea57b8abf14f8
Jihan Kim <jihan1218@gmail.com>
2020-04-06 16:09:16 -0700

    fixed spacing

--- pysc2/pysc2/agents/base_agent.js
+++ pysc2/pysc2/agents/base_agent.js
@@ -3,7 +3,7 @@ const actions = require(path.resolve(__dirname, '..', 'lib', './action.js'))
 
 class BaseAgent { //eslint-disable-line
 
-	constructor(){ //eslint-disable-line
+  constructor() { //eslint-disable-line
     this.reward = 0
     this.episodes = 0
     this.steps = 0
@@ -11,16 +11,16 @@ class BaseAgent { //eslint-disable-line
     this.action_spec = null
   }
 
-	setup(obs_spec, action_spec){ //eslint-disable-line
+  setup(obs_spec, action_spec) { //eslint-disable-line
     this.obs_spec = obs_spec
     this.action_spec = action_spec
   }
 
-	reset(){ //eslint-disable-line
+  reset() { //eslint-disable-line
     this.episodes += 1
   }
 
-  step(obs){ //eslint-disable-line
+  step(obs) { //eslint-disable-line
     this.steps += 1
     this.reward += obs.reward
     return actions.FunctionCall(actions.FUNCTIONS.no_op_id, [])
