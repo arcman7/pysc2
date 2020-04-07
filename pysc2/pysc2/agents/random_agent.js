552ebaffad7ffb454b60d65c7f887bb4439d30e5
Jihan Kim <jihan1218@gmail.com>
2020-04-07 13:47:02 -0700

    debug error

--- pysc2/pysc2/agents/random_agent.js
+++ pysc2/pysc2/agents/random_agent.js
@@ -5,14 +5,15 @@ const pythonUtils = require(path.resolve(__dirname, '..', 'lib', './.pythonUtils
 const randomChoice = pythonUtils
 
 class RandomAgent extends base_agent.BaseAgent {
+  // A random agent for starcraft. //
   step(obs) {
     const function_id = randomChoice(obs.observation.available_actions)
-    const args = Object.keys(this.action_spec.functions[function_id].args).forEach((key) => {
+    const args = []
+    Object.keys(this.action_spec.functions[function_id].args).forEach((key) => {
       const arg = this.action_spec.functions[function_id].args[key]
-      const list = []
       Object.keys(arg.sizes).forEach((k) => {
         const size = arg.sizes[k]
-        list.push(Math.floor(Math.random() * size))
+        args.push(Math.floor(Math.random() * size))
       })
     })
     return actions.FunctionCall(function_id, args)
