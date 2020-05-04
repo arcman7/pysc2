9fdd48b9a547ee64bc5cc2b11dd0d2100bc5cc1e
Jihan Kim <jihan1218@gmail.com>
2020-05-04 14:40:12 -0700

    initial pass

--- pysc2/pysc2/env/available_actions_printer.js
+++ pysc2/pysc2/env/available_actions_printer.js
@@ -1,14 +1,15 @@
 const path = require('path')
 const base_evn_wrapper = require(path.resolve(__dirname, './base_evn_wrapper.js'))
-
+/*An env wrapper to print the available actions.*/
 class AvailableActionsPrinter extends base_evn_wrapper.base_evn_wrapper {
+  /*An env wrapper to print the available actions.*/
   constructor(env) {
     super().__init__(env)
     this._seen = set()
     this._action_spec = this._action_spec()[0]
   }
-  step(args, kwargs) {
-    const all_obs = super.step(args, kwargs)
+  step() {
+    const all_obs = super.step(arguments)
     Object.keys(all_obs).forEach((key) => {
       const obs = all_obs[key]
       Object.keys(obs.observation["available_actions"]).forEach((key1) => {
