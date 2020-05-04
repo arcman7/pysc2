ef3f2f7c484953c70253fd23012bf07a1f6f916d
Jihan Kim <jihan1218@gmail.com>
2020-05-04 14:35:31 -0700

    initial pass

--- pysc2/pysc2/env/base_env_wrapper.js
+++ pysc2/pysc2/env/base_env_wrapper.js
@@ -6,7 +6,31 @@ class BaseEnvWrapper extends environment.Base {
     this._env = env
   }
 
-  close(args, kwargs) {
-    
+  close() {
+    return this._env.close(arguments)
+  }
+
+  action_spec() {
+    return this._env.action_spec(arguments)
+  }
+
+  observation_spec() {
+    return this._env.observation_spec(arguments)
+  }
+
+  reset() {
+    return this._env.reset(arguments)
+  }
+
+  step() {
+    return this._env.step(arguments)
+  }
+
+  save_replay() {
+    return this._env.save_replay(arguments)
+  }
+
+  get state() {
+    return this._env.state
   }
 }
