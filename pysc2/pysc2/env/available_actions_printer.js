dc70556065b4e45b76ce3939546dabcbe4677924
Jihan Kim <jihan1218@gmail.com>
2020-05-04 16:06:43 -0700

    save work

--- pysc2/pysc2/env/available_actions_printer.js
+++ pysc2/pysc2/env/available_actions_printer.js
@@ -5,17 +5,17 @@ class AvailableActionsPrinter extends base_evn_wrapper.base_evn_wrapper {
   /*An env wrapper to print the available actions.*/
   constructor(env) {
     super().__init__(env)
-    this._seen = set()
+    this._seen = new Set()
     this._action_spec = this._action_spec()[0]
   }
 
   step() {
-    const all_obs = super.step(arguments)
+    const all_obs = super.step(...arguments) //eslint-disable-line
     Object.keys(all_obs).forEach((key) => {
       const obs = all_obs[key]
       Object.keys(obs.observation["available_actions"]).forEach((key1) => {
         const avail = obs.observation["available_actions"][key1]
-        if (!(avail in this._seen)) {
+        if (!(this._seen.has(avail))) {
           this._seen.add(avail)
           this._print(this._action_spec.functions[avail].str(true))
         }
@@ -24,7 +24,7 @@ class AvailableActionsPrinter extends base_evn_wrapper.base_evn_wrapper {
     return all_obs
   }
 
-  _print(s) {
+  _print(s) { //eslint-disable-line
     console.log(s)
   }
 }
