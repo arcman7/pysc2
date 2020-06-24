d6421749162f4e2c5037c3c5977465852c7efcd9
Jihan Kim <jihan1218@gmail.com>
2020-06-23 21:09:30 -0700

    combine two files into one

--- pysc2/pysc2/lib/np_util.js
+++ pysc2/pysc2/lib/np_util.js
@@ -1,4 +1,5 @@
 const path = require('path') //eslint-disable-line
+const np = require(path.resolve(__dirname, './numpy.js'))
 const pythonUtils = require(path.resolve(__dirname, './pythonUtils.js'))
 const { arraySub, nonZero } = pythonUtils
 
@@ -19,6 +20,28 @@ function summarize_array_diffs(lhs, rhs) {
   return `${indices.length} element(s) changed - ${str}`
 }
 
+async function summarize_array_diffs_tf(lhs, rhs) {
+  // The same fucntion using tensorflow
+  if (Array.isArray(lhs)) {
+    lhs = np.tensor(lhs)
+  }
+  const diff = lhs.sub(rhs)
+  const mask = diff.abs().greater([0]).asType('bool')
+  const coords = (await np.whereAsync(mask)).arraySync()
+  if (coords.length === 0) {
+    return ''
+  }
+  const str = coords.map((coord) => {
+    if (coord.length === 1) {
+      return `[${coord[0]}]: ${np.getValueAt(lhs, coord)} -> ${np.getValueAt(rhs, coord)}`
+    }
+    const tempStr = coord.join('][')
+    return `[${tempStr}]: ${np.getValueAt(lhs, coord)} -> ${np.getValueAt(rhs, coord)}`
+  }).join('; ')
+  return `${coords.length} element(s) changed - ${str}`
+}
+
 module.exports = {
   summarize_array_diffs,
+  summarize_array_diffs_tf,
 }
