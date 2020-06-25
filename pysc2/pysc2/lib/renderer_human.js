b8cb331a77f9b3fbd4310765f3eba228cb4dfe2f
Andrew Carpenter <andrewryancarpenter77@gmail.com>
2020-06-24 21:19:27 -0700

    add simple http server

--- pysc2/pysc2/lib/renderer_human.js
+++ pysc2/pysc2/lib/renderer_human.js
@@ -1,16 +1,42 @@
 const { spawn } = require('child_process') //eslint-disable-line
 const path = require('path') //eslint-disable-line
+const http = require('http') //eslint-disable-line
+const url = require('url') // eslint-disable-line
+const gfile = require(path.resolve(__dirname, './gfile.js'))
 // const portspicker = require(path.resolve(__dirname, './portspicker.js'))
 
 const args = ['chrome.exe', '-incognito', '--new-window', 'http://127.0.0.1/']
 
 
 class HumanRenderer {
-  constructor(port) {
-    this.port = port
+  constructor(wsPort, httpPort) {
+    this.wsPort = wsPort
+    this.httpPort = httpPort
     this.args = args
-    const fullUrl = this.args.pop() + port
+    this.browserFilePath = '/human_renderer/browser.js'
+    const fullUrl = this.args.pop() + this.browserFilePath + '?port=' + wsPort
     this.args.push(fullUrl)
+    //await portspicker.pick_unused_ports(2)
+  }
+
+  async setUp() {
+    await this.hostJSScript()
+  }
+
+  hostJSScript() {
+    this.httpServer = http.createServer(function (request, response) {
+      const reqPath = url.parse(request.url, true).pathname;
+
+      if (request.method === 'GET') {
+        if (reqPath === this.browserFilePath) {
+          response.writeHead(200, { 'Content-Type': 'application/javascript' })
+          const data = gfile.Open(path.resolve(__dirname, 'human_renderer', 'browser.js'), { encoding: 'utf8' })
+          response.end(data, 'utf-8')
+        } else {
+          response.end(404)
+        }
+      }
+    })
   }
 
   launchChrome() {
