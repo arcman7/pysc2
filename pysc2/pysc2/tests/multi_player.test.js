158024e831952c06c684a8769edf5b76380d4395
Jihan Kim <jihan1218@gmail.com>
2020-08-24 21:05:49 -0700

    multi_player test init

---
+++ pysc2/pysc2/tests/multi_player.test.js
@@ -0,0 +1,15 @@
+// Test that multiplayer works independently of the SC2Env.
+
+const path = require('path')
+const s2clientprotocol = require('s2clientprotocol')
+const maps = require(path.resolve(__dirname, '..', 'maps'))
+const run_configs = require(path.resolve(__dirname, '..', 'run_configs'))
+const point = require(path.resolve(__dirname, '..', 'lib', 'point.js'))
+const portspicker = require(path.resolve(__dirname, '..', 'lib', 'portspicker.js'))
+const run_parallel = require(path.resolve(__dirname, '..', 'lib', 'run_parallel.js'))
+const utils = require(path.resolve(__dirname, './utils.js'))
+const sc_common = s2clientprotocol.common_pb
+const sc_pb = s2clientprotocol.sc2api_pb
+
+// const pythonUtils = require(path.resolve(__dirname, '..', 'lib', 'pythonUtils.js'))
+
