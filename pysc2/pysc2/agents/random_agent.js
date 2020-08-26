3ac9270b9d5c9deb19eae735e4de92e25f9afdd7
Andrew Carpenter <andrewryancarpenter77@gmail.com>
2020-08-25 19:40:53 -0700

    rename branch and take out comments

--- pysc2/pysc2/agents/random_agent.js
+++ pysc2/pysc2/agents/random_agent.js
@@ -8,19 +8,13 @@ class RandomAgent extends base_agent.BaseAgent {
   // A random agent for starcraft. //
   step(obs) {
     super.step(obs)
-    // console.log('random agent step: ', obs.observation.available_actions)
     const function_id = randomChoice(obs.observation.available_actions)
-    // console.log('function_id: ', function_id)
-    // console.log('this.action_spec.functions[function_id]:', this.action_spec.functions[function_id])
-    // console.log('this.action_spec.functions[function_id].args:', this.action_spec.functions[function_id].args)
-    // console.log('this.action_spec: ', this.action_spec)
     const args = []
     this.action_spec.functions[function_id].args.forEach((arg) => {
       args.push(arg.sizes.map((size) => { //eslint-disable-line
         return Math.floor(Math.random() * size)
       }))
     })
-    // console.log('actions.FunctionCall(function_id, args) - function_id: ', function_id, 'args: ', args)
     return new actions.FunctionCall(function_id, args)
   }
 }
