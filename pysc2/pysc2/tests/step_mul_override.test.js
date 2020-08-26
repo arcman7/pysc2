df60149ae7a3210ac59e63d2b33979cd4bb6e8f8
Jihan Kim <jihan1218@gmail.com>
2020-08-26 16:30:38 -0700

    fix withpythonasync

--- pysc2/pysc2/tests/step_mul_override.test.js
+++ pysc2/pysc2/tests/step_mul_override.test.js
@@ -4,7 +4,7 @@ const sc2_env = require(path.resolve(__dirname, '..', 'env', 'sc2_env.js'))
 const actions = require(path.resolve(__dirname, '..', 'lib', 'actions.js'))
 const utils = require(path.resolve(__dirname, './utils.js'))
 const pythonUtils = require(path.resolve(__dirname. '..', 'lib', 'pythonUtils.js'))
-const { assert } = pythonUtils
+const { assert, withPythonAsync } = pythonUtils
 
 const AGENT_INTERFACE_FORMAT = new sc2_env.AgentInterfaceFormat({
   feature_dimensions: new sc2_env.Dimensions(32, 32)
@@ -16,35 +16,38 @@ async function StepMulOverrideTest() {
   async function test_returns_game_loop_zero_on_first_step_despite_override() {
     console.log('=== test_returns_game_loop_zero_on_first_step_despite_override ===')
     testCase.setUp()
-    const env = new sc2_env.SC2Env({
+    const kwargs = {
       map_name: 'DefeatRoaches',
       players: [new sc2_env.Agent(Number(sc2_env.Race.random))],
       step_mul: 1,
       agent_interface_format: AGENT_INTERFACE_FORMAT
+    }
+    await withPythonAsync(sc2_env.SC2EnvFactory(kwargs), async (env) => {
+      const timestep = await env.step([actions.FUNCTIONS.no_op()], 1234)
+      assert(timestep[0].observation.game_loop[0] == 0, 'Error: timestep[0].observation.game_loop[0] == 0')
+      await testCase.tearDown()
     })
-    const timestep = await env.step([actions.FUNCTIONS.no_op()], 1234)
-    assert(timestep[0].observation.game_loop[0] == 0, 'Error: timestep[0].observation.game_loop[0] == 0')
-    await testCase.tearDown()
   }
   await test_returns_game_loop_zero_on_first_step_despite_override()
 
   async function test_respects_override() {
     console.log('=== test_respects_override ===')
     testCase.setUp()
-    const env = sc2_env.SC2Env({
+    const kwargs = {
       map_name: 'DefeatRoaches',
       players: [sc2_env.Agent(Number(sc2_env.Race.random))],
       step_mul: 1,
       agent_interface_format: AGENT_INTERFACE_FORMAT
-    })
-
-    let expected_game_loop = 0
-    for (let delta = 0; delta < 10; delta += 1) {
-      const timestep = await env.step([actions.FUNCTIONS.no_op()], delta)
-      expected_game_loop += delta
-      assert(timestep[0].observation.game_loop[0] == expected_game_loop, 'Error: timestep[0].observation.game_loop[0] == expected_game_loop')
     }
-    await testCase.tearDown()
+    await withPythonAsync(sc2_env.SC2EnvFactory(kwargs), async (env) => {
+      let expected_game_loop = 0
+      for (let delta = 0; delta < 10; delta += 1) {
+        const timestep = await env.step([actions.FUNCTIONS.no_op()], delta)
+        expected_game_loop += delta
+        assert(timestep[0].observation.game_loop[0] == expected_game_loop, 'Error: timestep[0].observation.game_loop[0] == expected_game_loop')
+      }
+      await testCase.tearDown()
+    })
   }
   await test_respects_override()
 }
