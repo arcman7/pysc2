8aafa9479f1b1037d0f5d4b6b712d813aa1f9eb4
Jihan Kim <jihan1218@gmail.com>
2020-09-02 13:49:02 -0700

    save work

--- pysc2/pysc2/tests/multi_player.test.js
+++ pysc2/pysc2/tests/multi_player.test.js
@@ -15,7 +15,7 @@ const pythonUtils = require(path.resolve(__dirname, '..', 'lib', 'pythonUtils.js
 const { assert, zip } = pythonUtils
 
 function print_stage(stage) {
-  console.info(`${stage}`.center(80, '-'))
+  console.info(` ${stage} `.center(80, '-'))
 }
 
 async function TestMultiplayer() {
@@ -24,7 +24,7 @@ async function TestMultiplayer() {
   testCase.setUp()
 
   async function test_multi_player() {
-    console.log('=== test_multi_player ===')
+    console.log('[ RUN      ] TestMultiplayer.test_multi_player')
     const players = 2
     const run_config = run_configs.get()
     const map_inst = maps.get('Simple64')
@@ -38,20 +38,22 @@ async function TestMultiplayer() {
     interfacee.setFeatureLayer(feature_layer)
     screen_size_px.assign_to(interfacee.getFeatureLayer().getResolution())
     minimap_size_px.assign_to(interfacee.getFeatureLayer().getMinimapResolution())
-
+    const proc_ports = await portspicker.pick_unused_ports(players)
     // Reserve a whole bunch of ports for the weird multiplayer implementation.
     const ports = await portspicker.pick_unused_ports(players * 2)
+
     console.info(`Valid Ports: ${ports}`)
 
     // Actually launch the game processes.
     print_stage('start')
-    const sc2_procs = []
+    let sc2_procs = []
     for (let i = 0; i < players; i += 1) {
       sc2_procs.push(run_config.start({
-        extra_ports: ports,
+        port: proc_ports.pop(),
         want_rgb: false
       }))
     }
+    sc2_procs = await Promise.all(sc2_procs)
     const controllers = []
     sc2_procs.forEach((p) => {
       controllers.push(p.controller)
@@ -61,9 +63,10 @@ async function TestMultiplayer() {
       // Save the maps so they can access it.
       const map_path = path.basename(map_inst.path)
       print_stage('save_map')
-      controllers.forEach((c) => { //Skip parallel due to a race condition on Windows.
-        c.save_map(map_path, map_inst.data(run_config))
-      })
+      for (let i = 0; i < controllers.length; i += 1) { //Skip parallel due to a race condition on Windows.
+        const c = controllers[i]
+        await c.save_map(map_path, map_inst.data(run_config))
+      }
 
       // Create the create request.
       const create = new sc_pb.RequestCreateGame()
@@ -96,56 +99,56 @@ async function TestMultiplayer() {
         print_stage('create')
         await controllers[0].create_game(create)
         print_stage('join')
-        await Promise.All(controllers.map((c) => c.join_game(join)))
+        await Promise.all(controllers.map((c) => c.join_game(join)))
 
         print_stage('run')
         for (let game_loop = 1; game_loop < 10; game_loop += 1) { //steps per episode
           // Step the game
-          await Promise.All(controllers.map((c) => c.step()))
+          await Promise.all(controllers.map((c) => c.step()))
 
           // Observe
-          const obs = await Promise.All(controllers.map((c) => c.observe()))
-          obs.forEach((p_id, o) => {
-            assert(o.observation.game_loop == game_loop, 'Error: o.observation.game_loop == game_loop')
-            assert(o.observation.player_common.player_id == p_id + 1, 'Error: o.observation.player_common.player_id == p_id + 1')
+          const obs = await Promise.all(controllers.map((c) => c.observe()))
+          obs.forEach((o, p_id) => {
+            assert(o.getObservation().getGameLoop() == game_loop, 'o.observation.game_loop == game_loop')
+            assert(o.getObservation().getPlayerCommon().getPlayerId() == p_id + 1, 'o.observation.player_common.player_id == p_id + 1')
           })
 
           // Act
           const actions = []
-          for (let i = 0; i < players; i += 1) {
+          for (let _ = 0; _ < players; _ += 1) {
             actions.push(new sc_pb.Action())
           }
-
           actions.forEach((action) => {
-            const pt = Math.floor(point.Point.unit_rand() * minimap_size_px)
+            const pt = point.Point.unit_rand().mul(minimap_size_px).floor()
             const actionfeaturelayer = new sc_pb.ObserverAction()
             const cammove = new spatial_pb.ActionSpatialCameraMove()
-            cammove.SetCenterMinimap(new sc_common.PointI())            
+            cammove.setCenterMinimap(new sc_common.PointI())
             actionfeaturelayer.setCameraMove(cammove)
             action.setActionFeatureLayer(actionfeaturelayer)
             pt.assign_to(action.getActionFeatureLayer().getCameraMove().getCenterMinimap())
           })
-          await Promise.All(zip(controllers, actions).map((c, a) => c.act(a)))
+          console.log('controllers: ', controllers)
+          console.log('actions: ', actions)
+          await Promise.all(zip(controllers, actions).map((c, a) => c.act(a)))
         }
       }
       // Done this game.
       print_stage('leave')
-      // parallel.run(c.leave for c in controllers)
-      await parallel.run()
-    }
-    finally {
+      await Promise.all(controllers.map((c) => c.leave))
+    } finally {
       print_stage('quit')
       // Done, shut down. Don't depend on parallel since it might be broken.
       controllers.forEach((c) => {
-        await c.quit()
+        c.quit()
       })
       sc2_procs.forEach((p) => {
-        await p.quit()
+        p.close()
       })
       await portspicker.return_ports(ports)
-    } 
+    }
 
     await testCase.tearDown()
+    console.log('[       OK ] TestMultiplayer.test_multi_player')
   }
   test_multi_player()
 }
